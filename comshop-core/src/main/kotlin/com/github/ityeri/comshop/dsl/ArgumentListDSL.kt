ec7ad6a33275342ba199448b182f0551b93d1ef5
it <ityeri9@gmail.com>
2026-01-07 20:15:16 +0900

    add: Feature that arguments define using infix

--- comshop-core/src/main/kotlin/com/github/ityeri/comshop/dsl/ArgumentListDSL.kt
+++ comshop-core/src/main/kotlin/com/github/ityeri/comshop/dsl/ArgumentListDSL.kt
@@ -8,7 +8,15 @@ import com.mojang.brigadier.arguments.ArgumentType
 class ArgumentListDSL {
     internal val arguments: MutableList<ArgumentNode> = mutableListOf()
 
-    operator fun <T> String.invoke(block: () -> ArgumentType<T>) {
+//    operator fun <T> String.invoke(block: () -> ArgumentType<T>) {
+//        arguments.add(SingleArgumentNode(block(), this))
+//    }
+
+    infix fun <T> String.to(argumentType: ArgumentType<T>) {
+        arguments.add(SingleArgumentNode(argumentType, this))
+    }
+
+    infix fun <T> String.to(block: () -> ArgumentType<T>) {
         arguments.add(SingleArgumentNode(block(), this))
     }
 
