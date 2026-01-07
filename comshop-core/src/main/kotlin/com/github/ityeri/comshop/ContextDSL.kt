13171ab0b8c0287e3b5b4e98378419eb458388c7
it <ityeri9@gmail.com>
2026-01-07 20:17:45 +0900

    refactor: Rename ContextWrapper -> ContextDSL

--- comshop-core/src/main/kotlin/com/github/ityeri/comshop/ContextDSL.kt
+++ comshop-core/src/main/kotlin/com/github/ityeri/comshop/ContextDSL.kt
@@ -3,7 +3,7 @@ package com.github.ityeri.comshop
 import com.mojang.brigadier.context.CommandContext
 import kotlin.reflect.KClass
 
-class ContextWrapper<S>(val context: CommandContext<S>) {
+class ContextDSL<S>(val context: CommandContext<S>) {
     fun <T : Any> getArg(name: String, clazz: KClass<T>): T {
         return context.getArgument<T>(name, clazz.java)
     }
