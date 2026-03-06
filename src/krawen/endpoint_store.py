53e1a9fb1f63f377701545e2bdc7c245b2b54c6e
--global <ityeri9@gmail.com>
2026-03-06 14:01:41 +0900

    edit: Change data property to private

--- src/krawen/endpoint_store.py
+++ src/krawen/endpoint_store.py
@@ -31,7 +31,7 @@ class EndpointStore(ABC): # TODO rename
 class JsonEndpointStore(EndpointStore):
     def __init__(self, json_file_path: str, file_store: AsyncFileStore):
         self.json_file_path: str = json_file_path
-        self.data: dict[EndpointPath, HTTPResponseInfo] = dict()
+        self._data: dict[EndpointPath, HTTPResponseInfo] = dict()
         self.file_store: AsyncFileStore = file_store
 
     type JsonResponseInfo = dict[str, str | tuple[tuple[str, str]]]
@@ -74,7 +74,7 @@ class JsonEndpointStore(EndpointStore):
         async with aiofiles.open(self.json_file_path, mode='r', encoding='utf-8') as f:
             text = await f.read()
             raw_data: dict[str, str] = json.loads(text)
-            self.data = {
+            self._data = {
                 self.str_to_endpoint_path(endpoint): self.json_to_response_info(json.loads(response_data))
                 for endpoint, response_data in raw_data.items()
             }
@@ -83,7 +83,7 @@ class JsonEndpointStore(EndpointStore):
         async with aiofiles.open(self.json_file_path, mode='w', encoding='utf-8') as f:
             json_data: dict[str, JsonEndpointStore.JsonResponseInfo] = {
                 self.endpoint_path_to_str(endpoint): self.response_info_to_json(response_info)
-                for endpoint, response_info in self.data.items()
+                for endpoint, response_info in self._data.items()
             }
             text = json.dumps(json_data, ensure_ascii=False, indent=indent)
             await f.write(text)
@@ -92,19 +92,19 @@ class JsonEndpointStore(EndpointStore):
         asyncio.create_task(self.save())
 
     async def put_endpoint(self, endpoint_path: EndpointPath, data: HTTPResponseData, auto_update: bool = True):
-        if not auto_update and endpoint_path in self.data:
+        if not auto_update and endpoint_path in self._data:
             raise DuplicateEndpointError()
 
-        self.data[endpoint_path] = data.info
+        self._data[endpoint_path] = data.info
         await self.file_store.put_file(self.endpoint_path_to_str(endpoint_path), data.body)
 
     async def rm_endpoint(self, endpoint_path: EndpointPath):
-        del self.data[endpoint_path]
+        del self._data[endpoint_path]
 
     async def get_endpoint(self, endpoint_path: EndpointPath) -> HTTPResponseData:
         try:
             return HTTPResponseData(
-                info=self.data[endpoint_path],
+                info=self._data[endpoint_path],
                 body=await self.file_store.get_file(self.endpoint_path_to_str(endpoint_path))
             )
         except KeyError:
