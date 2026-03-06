ce686dfd55bb970ca880c195b6c0ce9aa413a3b1
ityeri <ityeri9@gmail.com>
2026-03-06 21:26:29 +0900

    format: Remove comment

--- src/krawen/mirror_server.py
+++ src/krawen/mirror_server.py
@@ -19,7 +19,7 @@ async def default_not_found_handler(url: URL) -> str: pass
 class MirrorServer:
     def __init__(
             self,
-            root_origin_url: URL | str, # TODO commit, goooooooooooooo
+            root_origin_url: URL | str,
             source_store_path: str,
             url_manager: EndpointStore,
             not_found_handler: Callable[[URL], Awaitable[None]] = default_not_found_handler
