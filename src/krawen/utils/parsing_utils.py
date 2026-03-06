51c4dc650bb47ea0a97983a8d027c5fa35e15712
--global <ityeri9@gmail.com>
2026-03-06 13:50:07 +0900

    add: to_absolute_url func

--- src/krawen/utils/parsing_utils.py
+++ src/krawen/utils/parsing_utils.py
@@ -1,3 +1,13 @@
+from yarl import URL
+
+
+def to_absolute_url(origin_url: URL, target_url: URL) -> URL:
+    if target_url.is_absolute():
+        return target_url
+    else:
+        origin_url = origin_url.origin()
+        return origin_url.join(target_url)
+
 def parse_urls_from_tag_attr(key: str, value: str) -> list[str]:
     if isinstance(value, list):
         return value
