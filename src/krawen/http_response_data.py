efdc5938c8c66d720d21fd3d3cd6cf407fe4c041
it <ityeri9@gmail.com>
2026-03-06 09:11:11 +0900

    fix: Considering http key duplication feature, make HTTPResponseInfo can addressing duplicated keys

--- src/krawen/http_response_data.py
+++ src/krawen/http_response_data.py
@@ -8,17 +8,27 @@ class HTTPResponseInfo:
     http_version: str
     status_code: int
     reason: str
-    headers: dict[str, bytes]
+    headers: list[tuple[str, bytes]]
 
     def __post_init__(self):
-        self.headers = {key.lower(): value for key, value in self.headers.items()}
+        self.headers = [(key.lower(), value) for key, value in self.headers]
 
-    def get_header(self, key: str) -> bytes:
-        return self.headers[key.lower()]
+    def get_headers(self, key: str) -> list[bytes]:
+        return list(
+            map(
+                lambda header: header[1],
+                filter(lambda header: header[0] == key.lower(), self.headers)
+            )
+        )
+    def get_first_header(self, key: str) -> bytes:
+        try:
+            return self.get_headers(key)[0]
+        except IndexError:
+            raise KeyError('Passed key does not exist')
     @property
     def content_type(self) -> bytes | None:
         try:
-            return self.get_header("content-type")
+            return self.get_first_header('Content-Type')
         except KeyError:
             return None
 
