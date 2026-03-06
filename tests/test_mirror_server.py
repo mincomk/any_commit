01ecddca0b1c1bd0c3bebc3652e65b09e88abc5e
ityeri <ityeri9@gmail.com>
2026-03-06 22:06:04 +0900

    add: MirrorServer test code

---
+++ tests/test_mirror_server.py
@@ -0,0 +1,25 @@
+import asyncio
+import os
+
+from yarl import URL
+
+from krawen import MirrorServer
+from krawen.async_file_store import AsyncLocalFileStore
+from krawen.endpoint_store import JsonEndpointStore
+
+os.makedirs('./run/store', exist_ok=True)
+
+file_store = AsyncLocalFileStore('./run/store')
+endpoint_store = JsonEndpointStore('./run/endpoints.json', file_store=file_store)
+
+mirror_server = MirrorServer(
+    root_origin_url=URL('https://example.com'),
+    endpoint_store=endpoint_store
+)
+
+async def main():
+    await endpoint_store.load()
+    await mirror_server.start()
+
+if __name__ == '__main__':
+    asyncio.run(main())
